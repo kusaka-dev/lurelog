effc2ee548300924effc2b54c1aa22c017d9ce42
kusaka-dev <kusaka.aws@gmail.com>
2025-10-14 07:19:54 +0900

    headerを作成しました。

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { Sidebar } from "@/components/ui/sidebar";
+import { Header } from "@/components/ui/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,6 +19,7 @@ export const metadata: Metadata = {
   description: "ルアー管理・記録アプリケーション",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         <div className="flex">
           <Sidebar />
           <main className="flex-1 bg-gray-50">
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
 
 export function Header() {
   return (
     <>
-      <header className="bg-sky-500 w-500">
-        <div className="flex justify-between">
-          <h2 className="text-xl">ロゴ</h2>
-          <p>ヘッダーです。</p>
+      <header className="border-b-2 border-gray-200">
+        <div className="flex justify-between max-w-336 m-auto py-4">
+          <Image
+            src="/common/pic_logo.svg"
+            alt="lure.log"
+            width={111}
+            height={32}
+          />
+          <Image
+            src="/header/pic_profile.webp"
+            alt="プロフィール写真"
+            width={40}
+            height={40}
+          />
         </div>
       </header>
     </>
-  )
+  );
 }
